e39be390c67d3edb0cec9858c6b15d5e89adc514
Capellinx <capellaaa7@gmail.com>
2024-05-20 01:10:44 -0300

    refactor: refactoring the component and adding functionality

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,14 +1,19 @@
+import { ButtonHTMLAttributes, forwardRef } from "react"
 
-interface IButtonProps {
+interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
    text: string
+   newClass: string
 }
 
-export const Button = ({ text }: IButtonProps) => {
+export const Button = forwardRef<HTMLButtonElement, IButtonProps>(({ text, newClass, ...rest }, ref) => {
    return (
       <>
-         <button className="w-full bg-blue_b2bit text-lg font-bold font-nunito text-white p-2 rounded-lg">
+         <button
+            {...rest}
+            ref={ref}
+            className={`w-full bg-blue_b2bit text-lg font-bold font-nunito text-white p-2 rounded-lg ${newClass}`}>
             {text}
          </button>
       </>
    )
-}
+}) 
