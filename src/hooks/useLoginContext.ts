0e33c2482f6797f2eb1d24acae188849d3ed46e3
Capellinx <capellaaa7@gmail.com>
2024-05-20 17:15:46 -0300

    feat: updating message error in hook

--- src/hooks/useLoginContext.ts
+++ src/hooks/useLoginContext.ts
@@ -5,8 +5,8 @@ import { UserContext } from "../context/userContext"
 export const useLogin = () => {
    const userContext = useContext(UserContext)
 
-   if(!userContext){
-      throw new Error("The context not be able")
+   if (!userContext) {
+      throw new Error("useLogin must be used within a LoginProvider");
    }
 
    return userContext
