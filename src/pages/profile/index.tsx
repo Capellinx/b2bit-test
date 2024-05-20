52cd818f105f27d83ddcdbbb7edd046719e19147
Capellinx <capellaaa7@gmail.com>
2024-05-20 13:09:46 -0300

    feat putting component to render profile info

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from './../../components/header/index';
 import { Input } from './../../components/input/index';
+import { ProfileList } from './../../components/profileList/index';
 
 export const Profile = () => {
    return (
@@ -7,23 +8,8 @@ export const Profile = () => {
          <Header />
          <main className='bg-white_auth h-screen flex items-center justify-center'>
             <section className='bg-white w-md p-8 flex flex-col items-center rounded-2xl shadow-[0px_0px_40px_20px_#00002014]'>
-               <div>
-                  <p>Profile picture</p>
-               </div>
-               <div className='flex flex-col mt-8'>
-                  <Input
-                     label='Your name'
-                     placeholder='Lucas Capella'
-                     disabled
-                     className='w-[365px] bg-white_info p-4 mt-5 mb-6 rounded-lg'
-                  />
-                  <Input
-                     label='Your e-mail'
-                     placeholder='Lucas Capella'
-                     disabled
-                     className='w-[365px] bg-white_info p-4 mt-5 rounded-lg'
-                  />
-               </div>
+               <p>Profile picture</p>
+               <ProfileList />
             </section>
          </main>
       </>
