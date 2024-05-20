67c7624ab5089b83ab6af2dd29bff2cdf9732ab9
Capellinx <capellaaa7@gmail.com>
2024-05-20 03:16:30 -0300

    feat: create new routes (profile and 404)

--- src/routes/routesMain.tsx
+++ src/routes/routesMain.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import { Login } from "../pages/login"
+import { Profile } from "../pages/profile"
+import { NotFound } from "../pages/404"
 
 export const RoutesMain = () => {
    return(
       <Routes>
          <Route path="/" element={<Login/>}/>
+         <Route path="/profile" element={<Profile/>}/>
+         <Route path="*" element={<NotFound/>}/>
       </Routes>
    )
 }
