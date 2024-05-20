55de6a51914e05f86125fd65ae80f0d18d39962b
Capellinx <capellaaa7@gmail.com>
2024-05-20 03:15:36 -0300

    feat: create new keys and new interfaces

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -1,7 +1,18 @@
 export interface IUserContext {
    loginUser(data: IUserInformation): Promise<void>;
+   logoutUser(): void;
+   profile: IProfile[];
 }
 export interface IUserInformation {
    email: string;
    password: string;
+}
+export interface IProfile {
+   name: string;
+   email: string;
+   avatar: {
+      low: string,
+      midium: string;
+      high: string;
+   }
 }
