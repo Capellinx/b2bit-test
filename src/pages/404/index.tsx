8ee2638d62029d41bc1788b7c8e6f7afef40a192
Capellinx <capellaaa7@gmail.com>
2024-05-20 17:12:49 -0300

    fix: changing reference to image 404

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -3,7 +3,7 @@
 export const NotFound = () => {
    return(
       <section className="flex items-center justify-center flex-col h-screen">
-         <img src="/public/404-computer.svg" alt="" />
+         <img src="/public/404.svg" alt="" />
          <div>
             <p className="text-blue_b2bit text-2xl font-bold text-center">404 Not Found</p>
             <h2 className="text-4xl font-medium my-8">Whoops! That page doesn't exits</h2>
