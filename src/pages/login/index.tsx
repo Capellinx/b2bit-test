e8a862a3b297933dc5f86948f1bb8e50f3205578
Capellinx <capellaaa7@gmail.com>
2024-05-20 00:38:25 -0300

    feat: styling shadow on card

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,7 +4,7 @@ import { LoginForm } from './../../components/forms/loginForm/index';
 export const Login = () => {
    return (
       <main className='bg-white_light h-screen flex items-center justify-center flex-col rounded-lg'>
-         <section className='bg-white w-md p-8 flex flex-col items-center'>
+         <section className='bg-white w-md p-8 flex flex-col items-center rounded-lg shadow-[0px_0px_40px_20px_#00000024]'>
             <img
                src="/public/logo.png"
                alt="logo da b2bit"
