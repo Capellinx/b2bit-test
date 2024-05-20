9c808de4ee671161a2934377a0ac60bd03458195
Capellinx <capellaaa7@gmail.com>
2024-05-20 02:13:41 -0300

    refactor: fixing errors and implementing schema and connecting with the context

--- src/components/forms/loginForm/index.tsx
+++ src/components/forms/loginForm/index.tsx
@@ -1,21 +1,39 @@
 
 import { Input } from './../../input/index';
 import { Button } from './../../button/index';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { IUserInformation } from '../../../interfaces/user';
+import { loginSchema } from '../../../schemas/loginSchema';
+import { useLogin } from '../../../hooks/useLoginContext';
 
 export const LoginForm = () => {
+   const { loginUser } = useLogin()
+
+   const { handleSubmit, register, formState: { errors } } = useForm<IUserInformation>({
+      resolver: zodResolver(loginSchema)
+   })
+
+
    return (
-      <form className='flex flex-col mt-9'>
+      <form className='flex flex-col mt-9' onSubmit={handleSubmit(loginUser)}>
          <Input
-            label='E-mail' 
+            label='E-mail'
+            type='email'
             placeholder='@example.com'
-            className='w-[365px] bg-white_input p-4 mt-5 mb-5 rounded-lg'
+            className='w-[365px] bg-white_input p-4 mt-5  rounded-lg'
+            {...register('email')}
+            error={errors.email}
          />
          <Input
-            label='Password' 
+            label='Password'
+            type='password'
             placeholder='****************'
-            className='w-[365px] bg-white_input p-4 mt-5 mb-5 rounded-lg'
+            className='w-[365px] bg-white_input p-4 mt-5  rounded-lg'
+            {...register('password')}
+            error={errors.password}
          />
-         <Button text='Sig In'/>
+         <Button text='Sig In' type='submit' newClass='mt-8' />
       </form>
    )
 }
