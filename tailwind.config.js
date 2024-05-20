8895bbe132c94ae6fdbb368276d669c83895a5e2
Capellinx <capellaaa7@gmail.com>
2024-05-20 00:29:09 -0300

    chore: adding new colors for application

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      blue_b2bit: "##02274F",
+      blue_b2bit: "#02274F",
       yellow_b2bit: "#FDCF00",
       white_light: "#FAFAFA",
+      white_input: "#F1F1F1",
+      white_auth: "#F1F5F9",
       white: "#FFFFFF",
       black: "#262626",
     },
