41883d3d605b92e5958c34b2d944a353dff0a659
Capellinx <capellaaa7@gmail.com>
2024-05-20 17:13:03 -0300

    feat: adding react query and providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,28 @@
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'react-toastify/dist/ReactToastify.css';
 
 import { App } from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './context/userContext.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ToastContainer } from 'react-toastify'
 
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ToastContainer
+        position='top-center'
+        autoClose={1200}
+        pauseOnFocusLoss={false} />
+      <BrowserRouter>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
