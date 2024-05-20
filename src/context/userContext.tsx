b7c055aacdc21b01022150413edae72a61b38657
Capellinx <capellaaa7@gmail.com>
2024-05-20 03:15:56 -0300

    feat: improving context to do fetch and logout

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -1,24 +1,40 @@
 import { createContext, ReactNode, useState } from "react";
-import { IUserContext, IUserInformation } from "../interfaces/user";
+import { IProfile, IUserContext, IUserInformation } from "../interfaces/user";
 import { userService } from "../services/user";
 import { useNavigate } from "react-router-dom";
 
 export const UserContext = createContext({} as IUserContext)
 
-
 export const UserProvider = ({ children }: { children: ReactNode }) => {
    const navigate = useNavigate()
-
+   const [profile, setProfile] = useState<IProfile[]>([])
+   
    const loginUser = async ({ email, password }: IUserInformation) => {
-      const response = await userService.login({ email, password })
-      if (response) {
+      const { user } = await userService.login({ email, password })
+      if (user) {
+         const { email, name, avatar } = user
+
+         const newSchemaProfile: IProfile = {
+            email,
+            name,
+            avatar
+         }
+
+         setProfile([newSchemaProfile])
          navigate("/profile")
       }
    }
 
+   const logoutUser = () => {
+      localStorage.clear()
+      setProfile([])
+      navigate("/")
+   }
    return (
       <UserContext.Provider value={{
-         loginUser
+         loginUser,
+         profile,
+         logoutUser
       }}>
          {children}
       </UserContext.Provider>
