b1099687256decafe0a23b29507aba812feaee61
Capellinx <capellaaa7@gmail.com>
2024-05-20 17:16:43 -0300

    feat: adding protected in routeMain

--- src/routes/routesMain.tsx
+++ src/routes/routesMain.tsx
@@ -2,13 +2,16 @@ import { Route, Routes } from "react-router-dom"
 import { Login } from "../pages/login"
 import { Profile } from "../pages/profile"
 import { NotFound } from "../pages/404"
+import { ProtectedRoutes } from "../components/protectedRoutes"
 
 export const RoutesMain = () => {
-   return(
+   return (
       <Routes>
-         <Route path="/" element={<Login/>}/>
-         <Route path="/profile" element={<Profile/>}/>
-         <Route path="*" element={<NotFound/>}/>
+         <Route path="/" element={<Login />} />
+         <Route path="/profile" element={<ProtectedRoutes />}>
+            <Route index element={<Profile />} />
+         </Route>
+         <Route path="*" element={<NotFound />} />
       </Routes>
    )
 }
