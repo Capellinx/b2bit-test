38863800c7553d49a359dfd4f6b665e48ce0fc83
Capellinx <capellaaa7@gmail.com>
2024-05-20 17:13:33 -0300

    fix: changing text to children in button component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,18 +1,19 @@
-import { ButtonHTMLAttributes, forwardRef } from "react"
+import { ButtonHTMLAttributes, forwardRef, ReactNode } from "react"
 
 interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-   text: string
+   children: ReactNode
    newClass?: string
 }
 
-export const Button = forwardRef<HTMLButtonElement, IButtonProps>(({ text, newClass, ...rest }, ref) => {
+export const Button = forwardRef<HTMLButtonElement, IButtonProps>(({ children, newClass, ...rest }, ref) => {
    return (
       <>
          <button
             {...rest}
             ref={ref}
-            className={`w-full bg-blue_b2bit text-lg font-bold font-nunito text-white p-2 rounded-lg ${newClass}`}>
-            {text}
+            className={`w-full bg-blue_b2bit text-lg font-bold font-nunito text-white p-2 rounded-lg ${newClass}`}
+            >
+               {children}
          </button>
       </>
    )
