5640fcf0c785dab05b547c7e3045ccdd90075be5
Capellinx <capellaaa7@gmail.com>
2024-05-20 22:31:48 -0300

    fix: fixing data errors

--- src/components/profileList/profileCard/index.tsx
+++ src/components/profileList/profileCard/index.tsx
@@ -11,13 +11,13 @@ export const ProfileCard = ({ profile }: IProfileCardProps) => {
          <img src={profile.avatar.low} alt="" className="rounded-3xl mb-6  "/>
          <Input
             label='Your name'
-            placeholder={profile.email}
+            placeholder={profile.name}
             disabled
             className='w-[365px] bg-white_info p-4 mt-5 mb-6 rounded-lg'
          />
          <Input
             label='Your e-mail'
-            placeholder={profile.name}
+            placeholder={profile.email}
             disabled
             className='w-[365px] bg-white_info p-4 mt-5 rounded-lg'
          />
