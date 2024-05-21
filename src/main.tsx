a79bd2d024f695d9f69d864a5ed9c1216496b2f6
Capellinx <capellaaa7@gmail.com>
2024-05-20 22:09:40 -0300

    refactor: remove provider

--- src/main.tsx
+++ src/main.tsx
@@ -6,23 +6,20 @@ import 'react-toastify/dist/ReactToastify.css';
 import { App } from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './context/userContext.tsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ToastContainer } from 'react-toastify'
 
-const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ToastContainer
-        position='top-center'
-        autoClose={1200}
-        pauseOnFocusLoss={false} />
-      <BrowserRouter>
-        <UserProvider>
-          <App />
-        </UserProvider>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <ToastContainer
+      position='top-center'
+      autoClose={1200}
+      pauseOnFocusLoss={false} 
+    />
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
